6ca5aceaa92d570f4561f3b4923eaa0cdfdd4ec3
batson456 <batson456@gmail.com>
2012-08-02 08:24:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Port::Application.config.secret_token = 'c8cbbe1c4952330e4f6fe15a92876437ccdd4372985de9244ac5cb038304320c1f6bcea62d7196658a46dfc40ba6f1bd8ab16578a6ab4353524efcbd0a2f358f'
