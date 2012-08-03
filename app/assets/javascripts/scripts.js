a6a53b061777c8fcdb89c30592ddebb6c51a03dd
batson456 <batson456@gmail.com>
2012-08-03 12:11:37 -0400

    modify CSS, remove transitions, arrow works

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -3,6 +3,28 @@ $(document).ready(function(){
     function close(){
         $('.alert').alert('close');
     };
+
+    // hide #back-top first
+    $("#back-top").hide();
+    
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
 });
 
 // Twitter feed javascript
