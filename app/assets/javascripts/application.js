eea3cfdb9c5f8844fe73fef6c787b5e709df3409
batson456 <batson456@gmail.com>
2012-08-03 09:10:30 -0400

    Made major changes, added slider, and navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.easing
+//= require bootstrap
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Port::Application.routes.draw do
+  root :to => 'static_pages#home'
+
+  match 'home' => 'static_pages#home'
+  match 'about' => 'static_pages#about'
+  match 'contact' => 'static_pages#contact'
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
