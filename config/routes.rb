e38dbef4c82ee3510e32ce50b4368b7cc915665d
batson456 <batson456@gmail.com>
2012-08-04 09:44:33 -0400

    Remove redundant home from route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Port::Application.routes.draw do
   root :to => 'static_pages#home'
 
-  match 'home' => 'static_pages#home'
   match 'about' => 'static_pages#about'
   match 'contact' => 'static_pages#contact'
 
