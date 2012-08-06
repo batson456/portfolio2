4348bf48a8b48196a24b16e4ffc29690fd1be174
batson456 <batson456@gmail.com>
2012-08-05 21:58:00 -0400

    Add notificaion mailer and adjust config.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ActiveRecord::Base
   attr_accessible :email, :message, :name
+
+  validates :name, :email, :message, :presence => true
+  validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
+
+  def initialize(attributes = {})
+  	attributes.each do |name, value|
+  		send("#{name}=", value)
+  	end
+  end
 end
