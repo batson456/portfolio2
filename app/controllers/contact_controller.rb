9c861f64f2c7a2fff4be6b8d1f830f128f89f883
batson456 <batson456@gmail.com>
2012-08-06 11:19:03 -0400

    Fill out about page

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -8,7 +8,7 @@ class ContactController < ApplicationController
 
   	if @message.valid?
   		NotificationsMailer.send_message(@message).deliver
-  		flash[:success] = "Thanks! I'll get back to you as soon as I can"
+  		flash[:success] = "Thank you for your interest! I'll get back to you as soon as I can"
   		redirect_to root_url
   	end
   end
